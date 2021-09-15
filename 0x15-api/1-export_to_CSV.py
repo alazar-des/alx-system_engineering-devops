697e1428fa45eb1d0f2d8444082bcc36b10476bb
alazar-des <alazar.des@gmail.com>
2021-09-15 18:50:28 +0300

    user name fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,6 +8,11 @@ import sys
 
 
 if __name__ == "__main__":
+    emp_path = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    resp_emp = requests.get(emp_path)
+    resp_empj = resp_emp.json()
+    name = resp_empj.get("username")
+
     resp = requests.get('https://jsonplaceholder.typicode.com/todos')
     resp_json = resp.json()
     file_name = sys.argv[1] + ".csv"
@@ -16,5 +21,5 @@ if __name__ == "__main__":
                                 quoting=csv.QUOTE_ALL)
         for emp in resp_json:
             if emp.get("userId") == int(sys.argv[1]):
-                empl_write.writerow([emp.get("userId"), emp.get("id"),
+                empl_write.writerow([emp.get("userId"), name,
                                      emp.get("completed"), emp.get("title")])
