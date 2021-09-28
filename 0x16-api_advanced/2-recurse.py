b0bf7223fe9966ee4f15f95c1467b5d27dbdf831
alazar-des <alazar.des@gmail.com>
2021-09-28 20:13:48 +0300

    append recursively list fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,9 +8,9 @@ def append_title(child_list, hot_list=[]):
     """ append list recursively.
     """
     if len(child_list) == 1:
-        hot_list.append(child_list[0]['data']['title'])
         return hot_list
     else:
+        hot_list.append(child_list[0]['data']['title'])
         return append_title(child_list[1:], hot_list)
 
 
@@ -18,14 +18,14 @@ def recurse(subreddit, hot_list=[], after=""):
     """ Print the titles of the 10 hot posts.
     """
     endpoint = "https://www.reddit.com/r/" + subreddit +\
-        "/hot.json?limit=1&after=" + after
+        "/hot.json?after=" + after
     headers = {'User-Agent': 'Holberton/0.0.1'}
     res = requests.get(endpoint, headers=headers)
     if res.status_code == 200:
         res_json = res.json()
         hottest = res_json['data']
-        # append_title(hottest['children'], hot_list)
-        hot_list.append(hottest['children'][0]['data']['title'])
+        append_title(hottest['children'], hot_list)
+        # hot_list.append(hottest['children'][0]['data']['title'])
         if hottest['after'] is None:
             return hot_list
         else:
